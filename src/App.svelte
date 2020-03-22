7719428f62c1f0ceb57f476a9e923f9b0993646b
Chris Witte <cwitte@xmlhq.com>
2020-03-22 17:13:17 -0500

    back out App.svelte formatting change (from VsCode) it broke CSS!?!?!

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,26 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import { link } from 'svelte-spa-router'
-  import { push, pop, replace } from 'svelte-spa-router'
+import  Router from 'svelte-spa-router'
+import  {link} from 'svelte-spa-router'
+import {push, pop, replace} from 'svelte-spa-router'
 
-  import { store as AuthStore } from './stores/auth.js'
+import { store as AuthStore } from './stores/auth.js'
 
-  import RaceStandingList from './RaceStandingList.svelte'
-  import RacePhaseList from './RacePhaseList.svelte'
-  import DriverList from './DriverList.svelte'
-  import DriverAdd from './DriverAdd.svelte'
-  import EventSelection from './EventSelection.svelte'
-  import OrgSelection from './OrgSelection.svelte'
-  import ManualTimerAdd from './ManualTimerAdd.svelte'
-  import RaceStandingAdd from './RaceStandingAdd.svelte'
-  import Login from './Login.svelte'
-  import HotLoad from "./HotLoad.svelte";
-  //import CognitoAuth from "./CognitoAuth.svelte";
-  import { raceConfig } from './stores.js';
-  import { onMount } from 'svelte';
+import RaceStandingList from './RaceStandingList.svelte'
+import RacePhaseList from './RacePhaseList.svelte'
+import DriverList from './DriverList.svelte'
+import DriverAdd from './DriverAdd.svelte'
+import EventSelection from './EventSelection.svelte'
+import OrgSelection from './OrgSelection.svelte'
+import ManualTimerAdd from './ManualTimerAdd.svelte'
+import RaceStandingAdd from './RaceStandingAdd.svelte'
+import Login from './Login.svelte'
+import HotLoad from "./HotLoad.svelte";
+//import CognitoAuth from "./CognitoAuth.svelte";
+import { raceConfig} from './stores.js';
+import { onMount } from 'svelte';
 
 
-  const routes = {
+const routes = {
     // Exact path
     '/': RaceStandingList,
     '/RsList/:type': RaceStandingList,
@@ -32,13 +32,15 @@
     '/driverAdd': DriverAdd,
     '/eventSelection/:orgIz': EventSelection,
     '/orgSelection': OrgSelection,
-    // '/raceStandingAdd': RaceStandingAdd,
-  }
+   // '/raceStandingAdd': RaceStandingAdd,
+}
 
-  $: buildMenuMap($AuthStore)
-  let menuMap = [];
+$: buildMenuMap($AuthStore)
+  // empty menumap here seems to cause CSS issues!
+  let menuMap = [{ text: "Watch different event", clickHandler: () => navTo('/orgSelection'), alwaysShow: true },
+  ];
   const buildMenuMap = () => {
-    const loginLabel=  ($AuthStore && $AuthStore.username)?`Logout [${$AuthStore.username}]`:"Login"
+    const loginLabel = ($AuthStore && $AuthStore.username) ? `Logout [${$AuthStore.username}]` : "Login"
 
     menuMap = [
       { text: "Drivers", clickHandler: () => navTo('/drivers') },
@@ -50,89 +52,86 @@
     ]
   }
 
-
-  onMount(async () => {
+onMount(async () => {
     console.log("mounted app");
     buildMenuMap();
     replace('/orgSelection')
   });
-  const shouldDisplay = (menuOption, raceConfig) => {
-    if (menuOption.alwaysShow)
-      return true;
+const shouldDisplay=(menuOption, raceConfig)=>{
+  if(menuOption.alwaysShow )
+    return true;
 
-
-    return raceConfig.orgIz && raceConfig.orgId;
+  
+  return raceConfig.orgIz && raceConfig.orgId;
+}
+const getTitle=(cfg)=>{
+  if(cfg&&cfg.title) return cfg.title;
+  else
+  return "";
+}
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+const menuClickFunction=()=>{
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  const getTitle = (cfg) => {
-    if (cfg && cfg.title) return cfg.title;
-    else
-      return "";
-  }
-  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-  const menuClickFunction = () => {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-  const navTo = (route) => {
-    console.log("routing:" + route)
+}
+const navTo=(route)=>{
+    console.log("routing:"+route)
     menuClickFunction();
     replace(route);
-  }
+}
 </script>
 <style>
-  /* Style the navigation menu */
-  .topnav {
-    overflow: hidden;
-    background-color: #333;
-    position: relative;
-  }
+/* Style the navigation menu */
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+  position: relative;
+}
 
-  /* Hide the links inside the navigation menu (except for logo/home) */
-  .topnav #myLinks {
-    display: none;
-  }
+/* Hide the links inside the navigation menu (except for logo/home) */
+.topnav #myLinks {
+  display: none;
+}
 
-  /* Style navigation menu links */
-  .topnav a {
-    color: white;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-    display: block;
-  }
+/* Style navigation menu links */
+.topnav a {
+  color: white;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
 
-  /* Style the hamburger menu */
-  .topnav a.icon {
-    background: black;
-    display: block;
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-
-
+/* Style the hamburger menu */
+.topnav a.icon {
+  background: black;
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
 
 
-  /* Style the active link (or home/logo) */
-  .active {
-    background-color: #4CAF50;
-    color: white;
-  }
+
+
+/* Style the active link (or home/logo) */
+.active {
+  background-color: #4CAF50;
+  color: white;
+}
 </style>
-
 <body>
-  <!-- Top Navigation Menu -->
-  <div class="topnav">
-    <a href="#home" class="active">{getTitle($raceConfig)} Derby Race
-      <HotLoad /></a>
-    <!-- Navigation links (hidden by default) -->
-    <div id="myLinks">
+<!-- Top Navigation Menu -->
+<div class="topnav">
+  <a href="#home" class="active">{getTitle($raceConfig)} Derby Race <HotLoad/></a>
+  <!-- Navigation links (hidden by default) -->
+  <div id="myLinks">
 
-      {#each menuMap as menuOption}
+		{#each menuMap as menuOption}
       {#if shouldDisplay(menuOption, $raceConfig)}
        <a href="javascript:void(0);"  on:click={menuOption.clickHandler}>{menuOption.text}</a>
 			{/if}
