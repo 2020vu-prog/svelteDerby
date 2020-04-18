633ac562784fe35f63544439e46a558535545f4b
Connor Magnuson <connormagnuson@Connors-MacBook-Air.local>
2020-04-18 16:57:19 -0500

    populated jstree with chart types from s3

--- src/ChartAdd.svelte
+++ src/ChartAdd.svelte
@@ -1,119 +1,192 @@
 <script>
-    import { raceConfig } from './stores.js';
-    import { store } from './stores/auth.js'
-    import { Auth } from 'aws-amplify';
-    import { push, pop, replace } from 'svelte-spa-router'
-    import { onMount } from 'svelte';
+  import { raceConfig } from "./stores.js";
+  import { store } from "./stores/auth.js";
+  import { Auth } from "aws-amplify";
+  import { push, pop, replace } from "svelte-spa-router";
+  import { onMount } from "svelte";
 
-    import axios from "axios";
+  import axios from "axios";
 
-    var jsReady = false;
-    var treeReady = false;
-    var mounted = false;
-    var loginForm = {};
-    $:{syncAddButton(loginForm.chartName)}
-    const jqLoaded = () => {
-        console.log("jqloaded")
-        jsReady = true;
-        const jsTreeUrl="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/jstree.min.js"
-        jQuery.getScript( jsTreeUrl,jsTreeLoaded);
-        tryBuild();
-    };
-    const jsTreeLoaded = () => {
-        console.log("jstreeloaded")
-        treeReady = true;
-        tryBuild();
-
-    };
-    onMount(async () => {
-        mounted = true;
-        tryBuild();
-
-    });
-    const tryBuild = () => {
-        if (treeReady && mounted && jsReady) {
-            console.log("GO");
-            //  window.$(function () { window.$('#jstree_demo_div').jstree(); });
-
-            window.$('#jstree_demo_div').jstree({
-                'core': {
-                    'data': [
-                        { "id": "aasbd", "parent": "#", "text": "AASBD" },
-                        { "id": "ndr", "parent": "#", "text": "NDR" },
-                        { "id": "ndrDoubles", "parent": "ndr", "text": "Doubles" },
-                        { "id": "ndrDouble6", "parent": "ndrDoubles", "text": "6 Car" },
-                        { "id": "ndrSingles", "parent": "ndr", "text": "Singles" },
-                        { "id": "aasbdDoubles", "parent": "aasbd", "text": "Doubles" },
-                        { "id": "aasbdSingles", "parent": "aasbd", "text": "Singles" },
-                    ]
-                }
-            });
-            window.$('#jstree_demo_div').on("changed.jstree", function (e, data) {
-                console.log(data.selected);
-                loginForm.bracketSelected = data.selected;
-                syncAddButton();
-            });
+  var jsReady = false;
+  var treeReady = false;
+  var mounted = false;
+  var s3ChartTypes = false;
+  var loginForm = {};
+  $: {
+    syncAddButton(loginForm.chartName);
+  }
+  const jqLoaded = () => {
+    console.log("jqloaded");
+    jsReady = true;
+    const jsTreeUrl =
+      "https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/jstree.min.js";
+    jQuery.getScript(jsTreeUrl, jsTreeLoaded);
+    tryBuild();
+  };
+  const jsTreeLoaded = () => {
+    console.log("jstreeloaded");
+    treeReady = true;
+    tryBuild();
+  };
+  onMount(async () => {
+    mounted = true;
+    getChartDataFromServer();
+    tryBuild();
+  });
+  const tryBuild = () => {
+    if (treeReady && mounted && jsReady && s3ChartTypes) {
+      console.log("GO");
+      //  window.$(function () { window.$('#jstree_demo_div').jstree(); });
+      const testData = {
+        core: {
+          data: [
+            { id: "aasbd", parent: "#", text: "AASBD" },
+            { id: "ndr", parent: "#", text: "NDR" },
+            { id: "ndr/Doubles", parent: "ndr", text: "Doubles" },
+            { id: "ndrDouble6", parent: "ndr/Doubles", text: "6 Car" },
+            { id: "ndr/Singles", parent: "ndr", text: "Singles" },
+            { id: "aasbd/Doubles", parent: "aasbd", text: "Doubles" },
+            { id: "aasbd/Singles", parent: "aasbd", text: "Singles" }
+          ]
         }
+      };
+      const testData2 = { core: { data: [] } };
+      var keyList = getKeys(s3ChartTypes["Contents"]);
+      testData2.core.data = keyList;
+      window.$("#jstree_demo_div").jstree(testData2);
+      window.$("#jstree_demo_div").on("changed.jstree", function(e, data) {
+        console.log(data.selected);
+        loginForm.bracketSelected = data.selected;
+        syncAddButton();
+      });
     }
-    async function handleSubmit() {
-        console.log("Adding:" + JSON.stringify(loginForm))
-        const currentSession = await Auth.currentSession();
-        const bearer = currentSession.idToken.jwtToken;
+  };
+  async function handleSubmit() {
+    console.log("Adding:" + JSON.stringify(loginForm));
+    const currentSession = await Auth.currentSession();
+    const bearer = currentSession.idToken.jwtToken;
 
-        const req = {
-            orgId: $raceConfig.orgId,
-            orgIz: $raceConfig.orgIz,
-            bracket: loginForm.bracketSelected,
-            name: loginForm.chartName,
-        }
+    const req = {
+      orgId: $raceConfig.orgId,
+      orgIz: $raceConfig.orgIz,
+      bracket: loginForm.bracketSelected,
+      name: loginForm.chartName
+    };
 
-        console.log("token:" + bearer)
+    console.log("token:" + bearer);
 
-        axios.defaults.headers.common['Authorization'] = bearer;
+    axios.defaults.headers.common["Authorization"] = bearer;
 
-        axios.post($raceConfig.baseUrl + '/addChart', req)
-            .then((response) => {
-                console.log("addChart axios success")
-                pop();
-            })
-            .catch((err) => {
-                console.log("addChart failed: " + err)
-            })
-        loginForm.chartName = "";
-        loginForm.bracketSelected = "";
+    axios
+      .post($raceConfig.baseUrl + "/addChart", req)
+      .then(response => {
+        console.log("addChart axios success");
+        pop();
+      })
+      .catch(err => {
+        console.log("addChart failed: " + err);
+      });
+    loginForm.chartName = "";
+    loginForm.bracketSelected = "";
+  }
+  function syncAddButton() {
+    if (!mounted) {
+      return;
     }
-    function syncAddButton() {
-        if (!mounted) {
-            return;
-        }
-        if (loginForm.bracketSelected && loginForm.chartName) {
-            document.getElementById("formSubmitButton").disabled = false;
-            console.log("sync add button SYNC")
+    if (loginForm.bracketSelected && loginForm.chartName) {
+      document.getElementById("formSubmitButton").disabled = false;
+      console.log("sync add button SYNC");
+    } else {
+      console.log("sync add button FAIL");
+      document.getElementById("formSubmitButton").disabled = true;
+    }
+  }
+  // embedded script link: https://www.nielsvandermolen.com/external-javascript-sveltejs/
+  const getChartDataFromServer = async () => {
+    axios
+      .get(
+        $raceConfig.baseUrl +
+          "/listChartTypes?orgId=" +
+          $raceConfig.orgId +
+          "&orgIz=" +
+          $raceConfig.orgIz
+      )
+      .then(response => {
+        console.log("listChartTypes:" + response.data);
+        s3ChartTypes = response.data;
+        tryBuild();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  const getKeys = json => {
+    const children = [];
+    const parents = {};
+    json.forEach(function(item) {
+      var simpleKey = item.Key.replace("data/brackets/", "");
+      var structureArray = simpleKey.split("/");
+      var child = "";
+
+      //for (var i=structureArray.length-1;i>-1;i--) {}
+      while (structureArray.length > 0) {
+        if (!child) {
+          child = formatItem(structureArray);
+          children.push(child);
         } else {
-            console.log("sync add button FAIL")
-            document.getElementById("formSubmitButton").disabled = true;
+          const parent = formatItem(structureArray);
+            parents[parent.id]=parent;
         }
-    }
-// embedded script link: https://www.nielsvandermolen.com/external-javascript-sveltejs/
-
+        structureArray.pop();
+      }
+    });
+    const rc=[...Object.values(parents),...children]
+    rc.sort((a,b)=>{return a.id.length - b.id.length})
+    console.log(rc)
+    return rc
+  };
+  const formatItem = structureArray => {
+      console.log("format item ", structureArray);
+    const child = {};
+      child.id = structureArray.join("/");
+      child.text = structureArray[structureArray.length - 1];
+      if (structureArray.length > 1) {
+          const tempParents = [...structureArray];
+          tempParents.pop();  // just want the parents.
+        child.parent = tempParents.join("/");
+      } else {
+          child.parent = "#";
+      }
+      
+    return child;
+  };
 </script>
-<svelte:head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js" on:load={jqLoaded}></script>
 
+<svelte:head>
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js"
+    on:load={jqLoaded}>
+
+  </script>
 </svelte:head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css" />
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css" />
 
 <h3>Add Chart</h3>
 
 <form on:submit|preventDefault={handleSubmit}>
 
-    <label>
-        Chart Type:
-        <div id="jstree_demo_div"></div>
-    </label>
-    <label>
-        ChartName:
-        <input type="text" bind:value={loginForm.chartName} placeholder="Chart Name" />
-    </label>
-    <button id="formSubmitButton" type="submit" disabled>Add</button>
+  <label>
+    Chart Type:
+    <div id="jstree_demo_div" />
+  </label>
+  <label>
+    ChartName:
+    <input
+      type="text"
+      bind:value={loginForm.chartName}
+      placeholder="Chart Name" />
+  </label>
+  <button id="formSubmitButton" type="submit" disabled>Add</button>
 </form>
