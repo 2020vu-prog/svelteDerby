c742a633e0cf1ee051e8a678ab5941c9d81a6358
Chris Witte <cwitte@xmlhq.com>
2020-07-11 20:46:17 -0500

    No  incoming data announcements.

--- src/HotLoad.svelte
+++ src/HotLoad.svelte
@@ -324,6 +324,7 @@
     };
     onMount(async () => {});
     const announce = () => {
+        return; // no test announcement
         if ($autoAnnounceResults) {
             const incoming =
                 "https://cf.derby.rr1.us/archive/media.d6c3656e-4bfc-494e-8f6b-f28cdb8761df.mp3";
