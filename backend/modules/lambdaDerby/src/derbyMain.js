228edcbc49800aa76ed969e6afcdbba36710243e
Chris Witte <cwitte@xmlhq.com>
2020-09-04 07:07:19 -0500

    1.0.15

--- backend/modules/lambdaDerby/src/derbyMain.js
+++ backend/modules/lambdaDerby/src/derbyMain.js
@@ -983,7 +983,7 @@ const buildResponse = (jsonObj, cacheControl = "no-cache") => {
         headers: {
             "Content-Type": "application/json; charset=utf-8",
             "Cache-Control": cacheControl,
-            "x-client-minimum": "1.0.12",
+            "x-client-minimum": "1.0.15",
         },
         body: JSON.stringify(jsonObj),
     };
