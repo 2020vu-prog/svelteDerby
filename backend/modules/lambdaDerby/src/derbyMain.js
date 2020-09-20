83d93e13212b34c96088ba2bbda0a6cc44adcdc5
Chris Witte <cwitte@xmlhq.com>
2020-09-20 17:34:17 -0500

    bump versions to client: 1.1.3, and server: 1.1.1

--- backend/modules/lambdaDerby/src/derbyMain.js
+++ backend/modules/lambdaDerby/src/derbyMain.js
@@ -1,6 +1,6 @@
 "use strict";
-const clientMinimumVersion = "1.1.0";
-const derbyMainVersion = "1.1.0";
+const clientMinimumVersion = "1.1.3";
+const derbyMainVersion = "1.1.1";
 const crypto = require("crypto");
 const path = require("path");
 
