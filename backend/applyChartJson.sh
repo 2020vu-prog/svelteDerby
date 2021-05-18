00c83a7db6379de9ba72268a766a23bd89affd77
Chris Witte <cwitte@xmlhq.com>
2021-05-17 19:00:02 -0500

    24 car single json--rough draft for scouts

--- backend/applyChartJson.sh
+++ backend/applyChartJson.sh
@@ -1,5 +1,9 @@
 node  s3Utils/generateChartJson.js
 terraform taint null_resource.sync_s3_chart_data
-terraform apply -auto-approve
+terraform plan -target null_resource.sync_s3_chart_data -out /tmp/chart.out
+terraform apply /tmp/chart.out
+
+##### terraform apply -auto-approve
+
 now=$(date '+%s')
 sed "s/foo/$now/"<<< '{ "chartKey":"foo" }' > ../src/config/doNotEditChartKey.json
