27c3f74f9070707c09d89ff49ca51121c0c988c0
Connor Magnuson <ConnorM.9@outlook.com>
2022-05-17 21:20:35 -0500

    style improvements

--- frontend/src/DriverInfo.svelte
+++ frontend/src/DriverInfo.svelte
@@ -70,42 +70,108 @@
     onMount(async () => {
         bmdFromDexie = await db.BracketMetaData.toArray();
         bpListFromDexie = await db.BracketPos.toArray();
+        bpListFromDexie.sort((a, b) => {
+            return a.at - b.at;
+        });
         bpListFromDexie = bpListFromDexie.filter((a) => {
             return (
-                Object.keys(a.pos).length == 1 &&
+                Object.keys(a.pos).indexOf("A") == -1 &&
                 Object.values(a.pos)[0].ptcp == String(params.number)
             );
         });
     });
+    const placeMap = {
+        "": "Raced",
+        Place1: "1st Place",
+        Place2: "2nd Place",
+        Place3: "3rd Place",
+        Place4: "4th Place",
+        Place5: "5th Place",
+        Place6: "6th Place",
+        Place7: "7th Place",
+        Place8: "8th Place",
+    };
 
     function getNameFromBracketSK(bracketSK) {
         return bmdFromDexie.filter((a) => {
             return a.SK == bracketSK;
         })[0].bracketName;
     }
+
+    import { spring, tweened } from "svelte/motion";
+    import Pie from "./Pie.svelte";
+    var phaseWinPercentage;
+    $: phaseWinPercentage = (numPhasesWon / numPhasesRaced) * 100;
+    //const store = tweened(0, {duration: 1000});
+    const phaseStore = spring(0, { stiffness: 0.3, damping: 0.3 });
+    $: phaseStore.set(phaseWinPercentage);
+
+    var heatWinPercentage = 0;
+    $: {
+        if (bpListFromDexie) {
+            heatWinPercentage = (numHeatsWon / numHeatsRaced) * 100;
+        }
+    }
+    const heatStore = spring(0, { stiffness: 0.3, damping: 0.3 });
+    $: heatStore.set(heatWinPercentage);
 </script>
 
-<CarAndDriver
-    number={params.number}
-    at={safeGetAt($driverMap, params.number)}
-    isWinner=""
-    phaseLetter="" />
+<style>
+    .column {
+        float: left;
+        width: 50%;
+        text-align: center;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+</style>
+
+<div style="font-size: xx-large; width: 100%; text-align: center">
+    <CarAndDriver
+        number={params.number}
+        at={safeGetAt($driverMap, params.number)}
+        isWinner=""
+        phaseLetter="" />
+</div>
+<hr />
 <br />
 <br />
-Phases Won: {numPhasesWon++}/{racePhaseList.length}
-<br />
-Avg. Phase Win Time: {Math.round(phaseWinSum / numPhasesWon)} ms
-<br />
-<br />
-Heats Won: {numHeatsWon}/{numHeatsRaced}
-<br />
-Avg. Heat Win Time: {Math.round(heatWinSum / numHeatsWon)} ms
-<br />
-<br />
-{#if bpListFromDexie}
-    {#each bpListFromDexie as bp}
-        <strong>{getNameFromBracketSK(bp.SK.split(':')[0])}:</strong>
-        {bp.SK.split(':')[1]}
+<div class="row">
+    <div class="column">
+        <h2>Phases:</h2>
+        <Pie size={150} percent={$phaseStore} />
         <br />
-    {/each}
-{/if}
+        <strong>Phases Won:</strong>
+        {numPhasesWon}/{racePhaseList.length}
+        <br />
+        <strong>Avg. Win Time:</strong>
+        {Math.round(phaseWinSum / numPhasesWon)} ms
+    </div>
+    <div class="column">
+        <h2>Heats:</h2>
+        <Pie size={150} percent={$heatStore} />
+        <br />
+        <strong>Heats Won:</strong>
+        {numHeatsWon}/{numHeatsRaced}
+        <br />
+        <strong>Avg. Win Time:</strong>
+        {Math.round(heatWinSum / numHeatsWon)} ms
+        <br />
+    </div>
+</div>
+<hr />
+<div style="width: 100%; text-align: center">
+    <h2>Placements:</h2>
+    {#if bpListFromDexie && bpListFromDexie.length > 0}
+        {#each bpListFromDexie as bp}
+            <strong>{getNameFromBracketSK(bp.SK.split(':')[0])}:</strong>
+            {placeMap[bp.SK.split(':')[1]]}
+            <br />
+        {/each}
+    {:else}This racer has not yet participated in any complete brackets.{/if}
+</div>
