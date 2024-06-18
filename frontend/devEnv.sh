0ae1a7520c192aeede20e06a4ccb1305559990c5
Chris Witte <cwitte@xmlhq.com>
2024-06-18 12:50:32 -0500

    changes for ccm to run locally

--- frontend/devEnv.sh
+++ frontend/devEnv.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo tfde: ${TF_VAR_DeployEnvironment}
+
 source "./generatedTargets-${TF_VAR_DeployEnvironment}.sh"
-envsubst < webpack.config.js.template > webpack.config.${TF_VAR_DeployEnvironment}
- ln -sf webpack.config.${TF_VAR_DeployEnvironment} webpack.config.js
+if [[ -n "$DERBY_CLOUDFRONT" ]]
+then
+	envsubst < webpack.config.js.template > webpack.config.${TF_VAR_DeployEnvironment}
+fi
+
+
+ln -sf webpack.config.${TF_VAR_DeployEnvironment} webpack.config.js
 
 
 export now=$(date "+%s")
