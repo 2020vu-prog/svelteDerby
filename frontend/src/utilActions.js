6a501b8df09c24a12b25440ce9ed05502d48328c
Chris Witte <cwitte@xmlhq.com>
2024-07-19 07:10:20 -0500

    touch events

--- frontend/src/utilActions.js
+++ frontend/src/utilActions.js
@@ -15,17 +15,21 @@ export function longpress(node, threshold = 500) {
 			clearTimeout(timeout);
 			//node.removeEventListener('mousemove', cancel);
 			node.removeEventListener('mouseup', cancel);
+			node.removeEventListener('touchend', cancel);
 		};
 		
 		//node.addEventListener('mousemove', cancel);
 		node.addEventListener('mouseup', cancel);
+        node.addEventListener('touchend', cancel);
 	}
 	
 	node.addEventListener('mousedown', handle_mousedown);
+	node.addEventListener('touchstart', handle_mousedown);
 	
 	return {
 		destroy() {
 			node.removeEventListener('mousedown', handle_mousedown);
+			node.removeEventListener('touchstart', handle_mousedown);
 		}
 	};
 }
