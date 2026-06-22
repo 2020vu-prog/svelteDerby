9fe7af9bccc04b86f5a7b0fa9896d641c3fb3011
Chris Witte <cwitte@xmlhq.com>
2026-06-22 11:06:13 -0500

    Make annotations full width

--- frontend/src/Annotate.svelte
+++ frontend/src/Annotate.svelte
@@ -3,21 +3,26 @@
 </script>
 
 <style>
+    .annotationWrap {
+        width: 100%;
+    }
+
     .annotation {
         background-color: #fff3cd;
         border: 1px solid #b68200;
         color: #3d2b00;
-        display: inline-block;
+        display: block;
         font-family: inherit;
         font-size: 1.25rem;
         font-weight: 600;
         line-height: 1.2;
         padding: 0.12rem 0.35rem;
+        width: 100%;
     }
 </style>
 
 {#if text}
-    <div>
+    <div class="annotationWrap">
         <span class="annotation">{text}</span>
     </div>
 {/if}
