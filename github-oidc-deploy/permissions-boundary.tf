2b07cafc74be6d8afd85aeb5255e4b106b600e12
Chris Witte <cwitte@xmlhq.com>
2026-08-04 14:56:03 -0500

    Split GitHub OIDC Terraform configuration

---
+++ github-oidc-deploy/permissions-boundary.tf
@@ -0,0 +1,53 @@
+# This boundary prevents the deploy role from turning a managed Lambda or Cognito
+# role into an IAM principal with broader authority than the application needs.
+data "aws_iam_policy_document" "managed_role_boundary" {
+  statement {
+    sid    = "AllowApplicationRuntimeActions"
+    effect = "Allow"
+    actions = [
+      "cognito-identity:*",
+      "cognito-sync:*",
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:DescribeStream",
+      "dynamodb:GetItem",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:ListStreams",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem",
+      "ec2:RunInstances",
+      "iot:AttachPrincipalPolicy",
+      "iot:Connect",
+      "iot:Publish",
+      "iot:Receive",
+      "iot:Subscribe",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "mobileanalytics:PutEvents",
+      "polly:StartSpeechSynthesisTask",
+      "polly:SynthesizeSpeech",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListObjects",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "sns:Publish",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "managed_role_boundary" {
+  count = var.create_managed_role_permissions_boundary ? 1 : 0
+
+  name        = var.managed_role_permissions_boundary_name
+  description = "Maximum permissions for IAM roles created by svelteDerby Terraform."
+  policy      = data.aws_iam_policy_document.managed_role_boundary.json
+}
---
+++ github-oidc-deploy/shared.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+locals {
+  account_id                            = data.aws_caller_identity.current.account_id
+  partition                             = data.aws_partition.current.partition
+  github_oidc_url                       = "https://token.actions.githubusercontent.com"
+  github_oidc_host                      = "token.actions.githubusercontent.com"
+  github_subject                        = "repo:${var.github_owner}/${var.github_repo}:environment:${var.github_environment}"
+  oidc_provider_arn                     = var.create_oidc_provider ? aws_iam_openid_connect_provider.github_actions[0].arn : var.existing_oidc_provider_arn
+  managed_role_permissions_boundary_arn = var.create_managed_role_permissions_boundary ? aws_iam_policy.managed_role_boundary[0].arn : var.existing_managed_role_permissions_boundary_arn
+
+  app_bucket_arns = [
+    "arn:${local.partition}:s3:::svelte-static-*",
+    "arn:${local.partition}:s3:::svelte-cdn-logs*",
+    "arn:${local.partition}:s3:::derby-dst-bucket*",
+    "arn:${local.partition}:s3:::vod-lambda-src-*",
+  ]
+
+  state_bucket_arns = var.terraform_state_bucket_name == "" ? [] : [
+    "arn:${local.partition}:s3:::${var.terraform_state_bucket_name}",
+  ]
+
+  s3_bucket_arns = concat(local.app_bucket_arns, local.state_bucket_arns)
+  s3_object_arns = [for arn in local.s3_bucket_arns : "${arn}/*"]
+
+  terraform_lock_table_arns = var.terraform_lock_table_name == "" ? [] : [
+    "arn:${local.partition}:dynamodb:${var.aws_region}:${local.account_id}:table/${var.terraform_lock_table_name}",
+  ]
+}
