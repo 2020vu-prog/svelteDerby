c9d64584ddb1349c307816f7e7374cbaae586405
Chris Witte <cwitte@xmlhq.com>
2026-08-12 20:58:23 -0500

    Bust cached runtime config on frontend deploy (#30)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { initializeAwsConfig } from "./aws-config";
 
 async function startApp() {
-    const response = await axios.get("/app/getAwsConfig");
+    const response = await axios.get("/app/getAwsConfig", {
+        params: { cache: "[AIV]{date}[/AIV]" },
+    });
     initializeAwsConfig(response.data);
     const { default: App } = await import("./App.svelte");
 
