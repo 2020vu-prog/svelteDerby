e01e043357c4d71045496592f7168259f8deca0a
Chris Witte <cwitte@xmlhq.com>
2026-09-23 20:16:35 -0500

    Refresh SVG chart driver labels

--- frontend/src/ChartSvgPrototype.svelte
+++ frontend/src/ChartSvgPrototype.svelte
@@ -5,7 +5,7 @@
     import BracketSvg from "./BracketSvg.svelte";
     import ChartViewToggle from "./ChartViewToggle.svelte";
     import { getChartJson, augmentChartState } from "./utils.js";
-    import { spinnerPanelBusy } from "./stores.js";
+    import { doRefreshBlocks, driverMap, spinnerPanelBusy } from "./stores.js";
 
     export let params = {};
 
@@ -13,6 +13,40 @@
     let chartJson;
     let slotStates = {};
     let loadError = "";
+    let slotRefreshKey = "";
+    let slotLoadVersion = 0;
+
+    async function refreshSlotStates() {
+        if (!chartJson) return;
+
+        const loadVersion = ++slotLoadVersion;
+        const entries = await Promise.all(
+            Object.keys(chartJson.progress).flatMap((heatId) =>
+                ["A", "B"].map(async (slot) => [
+                    `${heatId}${slot}`,
+                    await augmentChartState(
+                        chartJson,
+                        params.chartId,
+                        heatId,
+                        slot
+                    ),
+                ])
+            )
+        );
+        if (loadVersion === slotLoadVersion) {
+            slotStates = Object.fromEntries(entries);
+        }
+    }
+
+    $: {
+        const refreshKey = chartJson
+            ? `${$doRefreshBlocks}:${Object.keys($driverMap).length}`
+            : "";
+        if (refreshKey && refreshKey !== slotRefreshKey) {
+            slotRefreshKey = refreshKey;
+            refreshSlotStates();
+        }
+    }
 
     onMount(async () => {
         $spinnerPanelBusy = true;
@@ -29,21 +63,6 @@
                 loadError = "Chart progression data was not found.";
                 return;
             }
-
-            const entries = await Promise.all(
-                Object.keys(chartJson.progress).flatMap((heatId) =>
-                    ["A", "B"].map(async (slot) => [
-                        `${heatId}${slot}`,
-                        await augmentChartState(
-                            chartJson,
-                            params.chartId,
-                            heatId,
-                            slot
-                        ),
-                    ])
-                )
-            );
-            slotStates = Object.fromEntries(entries);
         } catch (error) {
             loadError = "Unable to load this chart.";
         } finally {
