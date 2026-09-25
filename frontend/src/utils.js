6005f63b52dc102d6d2e62a3f5a1cf41769c0e93
Chris Witte <cwitte@xmlhq.com>
2026-09-25 15:28:50 -0500

    Remove unused SVG participant fallback

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -594,7 +594,6 @@ export async function augmentChartState(
     return {
         bracketClass,
         posHtml,
-        participant: position?.ptcp,
         isSeed,
         rsFromDexie,
     };
